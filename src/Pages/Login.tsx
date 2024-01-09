b98f2fa7ff0e1d4b32607dbcf24d264a07f5b896
David Mikeladze <davidmikeladze00@gmail.com>
2024-01-09 19:14:28 +0400

    feat: add some styles on login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,12 +4,20 @@ import logo from "../assets/logo.svg";
 export default function Login() {
   return (
     <div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center flex-col">
         <img src={logo} alt="" />
-        <div>
-          <h1>Login</h1>
-          <input type="text" placeholder="Email address" />
-          <input type="text" placeholder="Password" />
+        <div className="bg-[#161D2F]">
+          <h1 className="text-[#FFFFFF] text-[32px] font-light">Login</h1>
+          <input
+            className="bg-[#161D2F]"
+            type="text"
+            placeholder="Email address"
+          />
+          <input
+            className="bg-[#161D2F] border-b-[#5A698F] border-b"
+            type="text"
+            placeholder="Password"
+          />
           <button>Login to your account</button>
           <p>
             Don’t have an account?<span>Sign Up</span>
