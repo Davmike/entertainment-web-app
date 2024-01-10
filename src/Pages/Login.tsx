776e8cb183e63a362d08827a961b0ee844e23eb7
David Mikeladze <davidmikeladze00@gmail.com>
2024-01-10 15:02:02 +0400

    feat: add login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,23 +4,26 @@ import logo from "../assets/logo.svg";
 export default function Login() {
   return (
     <div>
-      <div className="flex justify-center items-center flex-col">
+      <div className="flex justify-center items-center flex-col min-h-screen">
         <img src={logo} alt="" />
-        <div className="bg-[#161D2F]">
+        <div className="bg-[#161D2F] rounded-[10px] w-[327px] h-[365px] mt-[58px] px-[24px] pt-[24px] pb-[32px]">
           <h1 className="text-[#FFFFFF] text-[32px] font-light">Login</h1>
           <input
-            className="bg-[#161D2F]"
+            className="bg-[#161D2F] border-b-[#5A698F] border-b mt-[40px] outline-none w-[279px] h-[37px]"
             type="text"
             placeholder="Email address"
           />
           <input
-            className="bg-[#161D2F] border-b-[#5A698F] border-b"
+            className="bg-[#161D2F] border-b-[#5A698F] border-b mt-[24px] outline-none w-[279px] h-[37px]"
             type="text"
             placeholder="Password"
           />
-          <button>Login to your account</button>
-          <p>
-            Don’t have an account?<span>Sign Up</span>
+          <button className="mt-[40px] w-[279px] h-[48px] bg-[#FC4747] rounded-[6px] text-[#FFFFFF] text-[13px] font-light leading-normal">
+            Login to your account
+          </button>
+          <p className="text-[#FFFFFF] text-[15px] font-light leading-normal text-center mt-[24px]">
+            Don’t have an account?
+            <span className="text-[#FC4747] ml-[8px]">Sign Up</span>
           </p>
         </div>
       </div>
