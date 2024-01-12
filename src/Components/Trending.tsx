3f4908143d160a55d4d46e4a41bc8de16b7c08bf
David Mikeladze <davidmikeladze00@gmail.com>
2024-01-12 16:00:54 +0400

    feat: add react slick slider

--- src/Components/Trending.tsx
+++ src/Components/Trending.tsx
@@ -1,48 +1,66 @@
 import movieImg from "../../public/assets/icon-category-movie.svg";
 import save from "../../public/assets/icon-bookmark-empty.svg";
 import MovieData from "../../data.json";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Trending() {
+  const settings = {
+    // dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
   return (
-    <div className="mt-[24px] pl-[16px]">
+    <div className="mt-[24px]">
       <div>
-        <h1 className="text-[#FFFFFF] text-[20px] font-light">Trending</h1>
-        {MovieData.map((movie, index) => {
-          console.log(movie.thumbnail); // Moved the console.log here
-          return movie.isTrending ? (
-            <div className="w-[240px] h-[140px] relative" key={index}>
-              <img
-                src={movie.thumbnail.trending?.small}
-                alt=""
-                className="mt-[16px] rounded-[8px]"
-              />
-              <div className="w-[32px] h-[32px] bg-[#596276] rounded-full flex justify-center items-center absolute top-[5%] left-[84%]">
-                <img src={save} alt="" />
-              </div>
-              <div className="pl-[16px] absolute top-[60%]">
-                <div className="flex items-center gap-[8px]">
-                  <p className="text-[12px] text-[white] font-light opacity-[0.75]">
-                    {movie.year}
-                  </p>
-                  <div className="bg-white rounded-full w-[3px] h-[3px] opacity-[0.5]"></div>
-                  <div className="flex items-center gap-[6px]">
-                    <img src={movieImg} alt="" />
+        <h1 className="text-[#FFFFFF] text-[20px] font-light pl-[16px]">
+          Trending
+        </h1>
+        <Slider {...settings}>
+          {MovieData.map((movie, index) => {
+            return movie.isTrending ? (
+              <div
+                className="w-[240px] h-[140px] relative mt-[16px] pl-[16px]"
+                key={index}
+              >
+                <img
+                  src={movie.thumbnail.trending?.small}
+                  alt=""
+                  className="rounded-[8px] w-[240px] h-[140px]"
+                />
+                <div className="w-[32px] h-[32px] bg-[#596276] rounded-full flex justify-center items-center absolute top-[5%] left-[84%]">
+                  <img src={save} alt="" />
+                </div>
+                <div className="pl-[16px] absolute top-[60%]">
+                  <div className="flex items-center gap-[8px]">
                     <p className="text-[12px] text-[white] font-light opacity-[0.75]">
-                      {movie.category}
+                      {movie.year}
+                    </p>
+                    <div className="bg-white rounded-full w-[3px] h-[3px] opacity-[0.5]"></div>
+                    <div className="flex items-center gap-[6px]">
+                      <img src={movieImg} alt="" />
+                      <p className="text-[12px] text-[white] font-light opacity-[0.75]">
+                        {movie.category}
+                      </p>
+                    </div>
+                    <div className="bg-white rounded-full w-[3px] h-[3px] opacity-[0.5]"></div>
+                    <p className="text-[12px] text-[white] font-light opacity-[0.75]">
+                      {movie.rating}
                     </p>
                   </div>
-                  <div className="bg-white rounded-full w-[3px] h-[3px] opacity-[0.5]"></div>
-                  <p className="text-[12px] text-[white] font-light opacity-[0.75]">
-                    {movie.rating}
-                  </p>
+                  <h2 className="text-[15px] text-[white] font-medium">
+                    Beyond Earth
+                  </h2>
                 </div>
-                <h2 className="text-[15px] text-[white] font-medium">
-                  Beyond Earth
-                </h2>
               </div>
-            </div>
-          ) : null;
-        })}
+            ) : null;
+          })}
+        </Slider>
       </div>
     </div>
   );
